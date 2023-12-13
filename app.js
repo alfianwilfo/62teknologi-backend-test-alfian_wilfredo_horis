6128aa47b00467421d17575b697f4eace37ba5af
Alfian Horis <alfian.horis@merahciptamedia.co.id>
2023-12-13 10:43:43 +0700

    checkpoint

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
-require('dotenv').config()
-const express = require('express')
-const app = express()
-const port = 3000
-const controller = require('./controller')
-var cors = require('cors')
+require("dotenv").config();
+const express = require("express");
+const app = express();
+var bodyParser = require("body-parser");
+var jsonParser = bodyParser.json();
 
-app.use(cors())
-app.get('/test', controller.test)
+const port = 3000;
+const controller = require("./controller");
+var cors = require("cors");
+
+app
+  .use(express.json())
+  .use(cors())
+  .use(bodyParser.urlencoded({ extended: false }))
+  .get("/test", controller.test)
+  .post("/business", controller.postBusiness);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
--- controller.js
+++ controller.js
@@ -1,5 +1,6 @@
 const axios = require("axios");
 const model = require("./config/table");
+const Validator = require("validatorjs");
 const Helper = require("./helpers");
 exports.test = async (req, res, next) => {
   try {
@@ -14,111 +15,263 @@ exports.test = async (req, res, next) => {
     };
     axios(options)
       .then(async function (response) {
-        const data = response.data.businesses;
-        if (data.length) {
-          await model.query(`START TRANSACTION`);
-          for await (const d of data) {
-            await model.execute(`
-            INSERT INTO businesses(id, alias, name, image_url, review_count, rating, price, phone, distance, display_phone) VALUES
-            (
-              "${d.id}",
-              "${d.alias}",
-              "${d.name}",
-              "${d.image_url}",
-              "${d.review_count}",
-              "${d.rating}",
-              "${d.price}",
-              "${d.phone}",
-              "${d.distance}",
-              "${d.display_phone}"
-            )
-            `);
-            if (d.categories.length) {
-              for await (const c of d.categories) {
-                let [check] = await model.query(
-                  `SELECT * FROM categories WHERE alias = "${c.alias}" LIMIT 1`
-                );
-                if (!check.length) {
-                  await model.execute(`
-                  INSERT INTO categories(id, alias, title) VALUES
-                  (
-                    "${Helper.getId()}",
-                    "${c.alias}",
-                    "${c.title}"
-                  )
-                  `);
-                  let [data] = await model.query(
+        try {
+          const data = response.data.businesses;
+          if (data.length) {
+            await model.query(`START TRANSACTION`);
+            for await (const d of data) {
+              await model.execute(`
+                INSERT INTO businesses(id, alias, name, image_url, review_count, rating, price, phone, distance, display_phone) VALUES
+                (
+                  "${d.id}",
+                  "${d.alias}",
+                  "${d.name}",
+                  "${d.image_url}",
+                  "${d.review_count}",
+                  "${d.rating}",
+                  "${d.price}",
+                  "${d.phone}",
+                  "${d.distance}",
+                  "${d.display_phone}"
+                )
+                `);
+              if (d.categories.length) {
+                for await (const c of d.categories) {
+                  let [check] = await model.query(
                     `SELECT * FROM categories WHERE alias = "${c.alias}" LIMIT 1`
                   );
-                  await model.execute(`
-                  INSERT INTO bussinesses_categories(id, business_id, category_id) VALUES
-                  (
-                    "${Helper.getId()}",
-                    "${d.id}",
-                    "${data[0].id}"
-                  )
-                  `);
-                } else {
-                  await model.execute(`
-                    INSERT INTO bussinesses_categories(id, business_id, category_id) VALUES
-                    (
+                  if (!check.length) {
+                    await model.execute(`
+                      INSERT INTO categories(id, alias, title) VALUES
+                      (
+                        "${Helper.getId()}",
+                        "${c.alias}",
+                        "${c.title}"
+                      )
+                      `);
+                    let [data] = await model.query(
+                      `SELECT * FROM categories WHERE alias = "${c.alias}" LIMIT 1`
+                    );
+                    await model.execute(`
+                      INSERT INTO bussinesses_categories(id, business_id, category_id) VALUES
+                      (
                         "${Helper.getId()}",
                         "${d.id}",
-                        "${check[0].id}"
-                    )
-                    `);
-                }
-              }
-            }
-            if (d.transactions.length) {
-              for await (let t of d.transactions) {
-                let [check] = await model.query(
-                  `SELECT * FROM transactions WHERE name = "${t}" LIMIT 1`
-                );
-                if (!check.length) {
-                  await model.execute(`
-                        INSERT INTO transactions(id, name) VALUES
-                        (
-                            "${Helper.getId()}",
-                            "${t}"
-                        )
-                        `);
-                  let [data] = await model.query(
-                    `SELECT * FROM transactions WHERE name = "${t}" LIMIT 1`
-                  );
-                  await model.execute(`
-                        INSERT INTO bussinesses_transactions(id, business_id, transaction_id) VALUES
-                        (
-                            "${Helper.getId()}",
-                            "${d.id}",
-                            "${data[0].id}"
-                        )
-                        `);
-                } else {
-                  await model.execute(`
-                        INSERT INTO bussinesses_transactions(id, business_id, transaction_id) VALUES
+                        "${data[0].id}"
+                      )
+                      `);
+                  } else {
+                    await model.execute(`
+                        INSERT INTO bussinesses_categories(id, business_id, category_id) VALUES
                         (
                             "${Helper.getId()}",
                             "${d.id}",
                             "${check[0].id}"
                         )
                         `);
+                  }
+                }
+              }
+              if (d.transactions.length) {
+                for await (let t of d.transactions) {
+                  let [check] = await model.query(
+                    `SELECT * FROM transactions WHERE name = "${t}" LIMIT 1`
+                  );
+                  if (!check.length) {
+                    await model.execute(`
+                            INSERT INTO transactions(id, name) VALUES
+                            (
+                                "${Helper.getId()}",
+                                "${t}"
+                            )
+                            `);
+                    let [data] = await model.query(
+                      `SELECT * FROM transactions WHERE name = "${t}" LIMIT 1`
+                    );
+                    await model.execute(`
+                            INSERT INTO bussinesses_transactions(id, business_id, transaction_id) VALUES
+                            (
+                                "${Helper.getId()}",
+                                "${d.id}",
+                                "${data[0].id}"
+                            )
+                            `);
+                  } else {
+                    await model.execute(`
+                            INSERT INTO bussinesses_transactions(id, business_id, transaction_id) VALUES
+                            (
+                                "${Helper.getId()}",
+                                "${d.id}",
+                                "${check[0].id}"
+                            )
+                            `);
+                  }
                 }
               }
             }
           }
+          await model.execute(`COMMIT;`);
+
+          res.json({
+            code: 200,
+            status: "success",
+            message: "success",
+            result: [],
+          });
+        } catch (error) {
+          await model.execute(`ROLLBACK;`);
+
+          console.log(error);
         }
-        res.json({
-          code: 200,
-          status: "success",
-          message: "success",
-          result: [],
-        });
-        await model.execute(`COMMIT;`);
       })
       .catch(async function (error) {
-        await model.execute(`ROLLBACK;`);
         console.error(error, "errorss");
       });
   } catch (error) {}
+};
+
+exports.postBusiness = async (req, res, next) => {
+  let {
+    alias,
+    name,
+    image_url,
+    price,
+    phone,
+    display_phone,
+    categories,
+    transactions,
+  } = req.body;
+  let id = Helper.getId();
+  let rules = {
+    alias: "required",
+    name: "required",
+    image_url: "required",
+    price: "required|check_price",
+    phone: "required",
+    display_phone: "required",
+    categories: "required|check_categories",
+  };
+
+  let error_msg = {
+    required: ":attribute cannot be null",
+    in: "invalid :attribute",
+  };
+
+  let validation = new Validator(
+    {
+      alias,
+      name,
+      image_url,
+      price,
+      phone,
+      display_phone,
+      categories,
+    },
+    rules,
+    error_msg
+  );
+  Validator.registerAsync(
+    "check_price",
+    function (price, attribute, req, passes) {
+      if (price == "$" || price == "$$" || price == "$$$" || price == "$$$$") {
+        passes();
+      } else {
+        passes(false, "Invalid price.");
+      }
+    }
+  );
+  Validator.registerAsync(
+    "check_categories",
+    function (categories, attribute, req, passes) {
+      if (Array.isArray(categories)) {
+        if (categories.length) {
+          let check = true;
+          categories.forEach((e) => {
+            let { alias, title } = e;
+            if (!alias || !title) {
+              check = false;
+            }
+          });
+          if (!check) {
+            console.log("disini");
+            passes(false, "Invalid categories.");
+          } else {
+            passes();
+          }
+        } else {
+          passes(false, "business must have at least one categories.");
+        }
+      } else {
+        passes(false, "Invalid categories.");
+      }
+    }
+  );
+  validation.checkAsync(passes, fails);
+  function fails() {
+    let message = [];
+    for (var key in validation.errors.all()) {
+      var value = validation.errors.all()[key];
+      message.push(value[0]);
+    }
+    return res.status(200).json({
+      code: 401,
+      status: "error",
+      message: message,
+      result: [],
+    });
+  }
+  async function passes() {
+    try {
+      await model.query(`START TRANSACTION;`);
+      await model.execute(`
+        INSERT INTO businesses(id, alias, name, image_url, price, phone, display_phone) VALUES
+        (
+            "${id}",
+            "${alias}",
+            "${name}",
+            "${image_url}",
+            "${price}",
+            "${phone}",
+            "${display_phone}"
+        )
+        `);
+      if (categories.length) {
+        for await (let c of categories) {
+          let [check] = await model.query(
+            `SELECT * FROM categories WHERE title = "${c.title}" LIMIT 1`
+          );
+          if (!check.length) {
+            await model.execute(`
+                        INSERT INTO categories(id, alias, title) VALUES
+                        (
+                            "${Helper.getId()}",
+                            "${c.alias}",
+                            "${c.title}"
+                        )
+                        `);
+            let [data] = await model.query(
+              `SELECT * FROM categories WHERE title = "${c.title}" LIMIT 1`
+            );
+            await model.execute(`
+                        INSERT INTO bussinesses_categories(id, business_id, category_id) VALUES
+                        (
+                            "${Helper.getId()}",
+                            "${id}",
+                            "${data[0].id}"
+                        )
+                        `);
+          }
+        }
+      }
+      await model.execute(`COMMIT;`);
+      res.json({
+        code: 201,
+        status: "success",
+        message: "success",
+        result: [],
+      });
+    } catch (error) {
+      console.log(error);
+    }
+  }
 };
